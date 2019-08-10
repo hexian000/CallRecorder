fbe1c1330ada771012977fff25df20f5fdfab045
hexian000 <he.xian@163.com>
2019-08-10 09:56:14 +0800

    better UI experience

--- app/src/main/java/me/hexian000/callrecorder/MainActivity.java
+++ app/src/main/java/me/hexian000/callrecorder/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -14,7 +15,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends Activity {
-	private static final int PERMISSIONS_REQUEST_CODE = 0;
+	private static final int PERMISSIONS_REQUEST_CALL = 0;
+	private static final int PERMISSIONS_REQUEST_MIC = 1;
 	private static final String[] CALL_RECORD_PERMISSIONS = new String[]{
 			Manifest.permission.CAPTURE_AUDIO_OUTPUT,
 			Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -23,7 +25,7 @@ public class MainActivity extends Activity {
 			Manifest.permission.READ_CONTACTS,
 			Manifest.permission.READ_CALL_LOG,
 	};
-	private static final String[] RECORD_PERMISSIONS = new String[]{
+	private static final String[] MIC_RECORD_PERMISSIONS = new String[]{
 			Manifest.permission.WRITE_EXTERNAL_STORAGE,
 			Manifest.permission.RECORD_AUDIO,
 	};
@@ -64,11 +66,10 @@ public class MainActivity extends Activity {
 	public void ToggleCallRecord(View v) {
 		final ToggleButton toggle = (ToggleButton) v;
 		if (toggle.isChecked()) {
-			grantPermissions(checkPermissions(CALL_RECORD_PERMISSIONS));
-
-			if (checkPermissions(CALL_RECORD_PERMISSIONS).length > 0) {
+			final String[] perms = checkPermissions(CALL_RECORD_PERMISSIONS);
+			if (perms.length > 0) {
 				toggle.setChecked(false);
-				Toast.makeText(this, R.string.no_permission, Toast.LENGTH_SHORT).show();
+				grantPermissions(PERMISSIONS_REQUEST_CALL, perms);
 				return;
 			}
 
@@ -78,14 +79,13 @@ public class MainActivity extends Activity {
 		}
 	}
 
-	public void ToggleRecord(View v) {
+	public void ToggleMicRecord(View v) {
 		final ToggleButton toggle = (ToggleButton) v;
 		if (toggle.isChecked()) {
-			grantPermissions(checkPermissions(RECORD_PERMISSIONS));
-
-			if (checkPermissions(RECORD_PERMISSIONS).length > 0) {
+			final String[] perms = checkPermissions(MIC_RECORD_PERMISSIONS);
+			if (perms.length > 0) {
 				toggle.setChecked(false);
-				Toast.makeText(this, R.string.no_permission, Toast.LENGTH_SHORT).show();
+				grantPermissions(PERMISSIONS_REQUEST_MIC, perms);
 				return;
 			}
 
@@ -109,9 +109,34 @@ public class MainActivity extends Activity {
 		return permissions.toArray(new String[]{});
 	}
 
-	private void grantPermissions(final String[] permissions) {
+	private void grantPermissions(final int code, final String[] permissions) {
 		if (permissions.length > 0) {
-			requestPermissions(permissions, PERMISSIONS_REQUEST_CODE);
+			requestPermissions(permissions, code);
+		}
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+	                                       @NonNull int[] grantResults) {
+		for (int result : grantResults) {
+			if (result != PackageManager.PERMISSION_GRANTED) {
+				Toast.makeText(this, R.string.no_permission, Toast.LENGTH_SHORT).show();
+				return;
+			}
+		}
+		switch (requestCode) {
+		case PERMISSIONS_REQUEST_CALL: {
+			final ToggleButton toggle = findViewById(R.id.toggleCallRecord);
+			toggle.setChecked(true);
+			ToggleCallRecord(toggle);
+		}
+		break;
+		case PERMISSIONS_REQUEST_MIC: {
+			final ToggleButton toggle = findViewById(R.id.toggleMicRecord);
+			toggle.setChecked(true);
+			ToggleMicRecord(toggle);
+		}
+		break;
 		}
 	}
 }
